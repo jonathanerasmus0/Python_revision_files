211a9c403bc1960a9fea1114f52012e890498aee
Jonathan Davies <jonathan.davies@dci-student.org>
2024-02-09 10:58:59 +0100

    python_revision_files

---
+++ function_practice_exercises.py
@@ -0,0 +1,31 @@
+# Write a function that returns the lesser of two numebrs if both numbers are even
+#but returns the greater if one or both numbers are odd
+
+def lesser_of_two_evens(a,b):
+    if a%2 == 0 and b%2 == 0: # both numbers here are even
+        if a < b :
+            result = a
+        else:
+            result = b
+    else:
+        if a > b :
+            result = a
+        else:
+            result = b
+        return result
+
+# animal crackers : Write a function which takes a two word sting and 
+#returns true if both words start with the same letter
+
+def animal_crackers(text):
+    
+            
+            
+
+    
+            
+	
+     
+    
+		     
+  
