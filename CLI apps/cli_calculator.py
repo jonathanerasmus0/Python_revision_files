84a8d3e6e51584c2604ec907b928ddc98dc31869
Jonathan Davies <jonathan.davies@dci-student.org>
2024-02-12 13:42:10 +0100

    cli

---
+++ CLI apps/cli_calculator.py
@@ -0,0 +1,14 @@
+import sys
+
+
+print(sys.argv[1:])
+args = sys.argv[1:]
+
+# m+n 
+
+m = int.args[0]
+op = str.args[1]
+n = int(args[2])
+
+
+
---
+++ CLI apps/cli_nizar.py
@@ -0,0 +1,15 @@
+import sys
+arguments  = sys.argv
+
+print(arguments)
+
+
+
+
+
+def say_hello():
+    print("hello people")
+    
+if "hello" in arguments:
+    say_hello()
+    
