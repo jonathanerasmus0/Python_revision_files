4dda40c50ddd15c6fd030ebab9a40e5ef39345a1
Jonathan Davies <jonathan.davies@dci-student.org>
2024-02-13 14:03:46 +0100

    Json

--- JSON/json.py
+++ JSON/json.py
@@ -1,8 +1,21 @@
-#javascript Object Notation
 import json,os
-from abc import ABC,abstractmethod
-
+from abc import ABC, abstractmethod
 full_path = os.path.abspath(__file__)
+directory, _file = os.path.split(full_path)
+files: list = [
+    os.path.join(directory, "data.json"),
+    os.path.join(directory, "data.txt"),
+    os.path.join(directory, "data.log")
+]
+
+
+
+    
+    
+    
+  
+	
+ 
 
 
 
