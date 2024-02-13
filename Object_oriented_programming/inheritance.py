88b0ade5db1f68a67b9b165594c5d02550dec562
Jonathan Davies <jonathan.davies@dci-student.org>
2024-02-13 10:56:58 +0100

    OOP

--- Object_oriented_programming/inheritance.py
+++ Object_oriented_programming/inheritance.py
@@ -13,12 +13,44 @@ class Employee:
         self.pay = int(self.pay * self.raise_amt)
 
 class Developer(Employee): # developer class is now inheriting from Employee class
-    pass
+    raise_amt = 1.10
+    
+    def __init__(self,first,last,pay,prog_lang):
+        super().__init__(first,last,pay)
+        self.prog_lang = prog_lang
+class Manager(Employee):
+    def __init__(self,first,last,pay,employees=None):
+        super().__init__(first,last,pay)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+    def add_emp(self,emp):
+        if emp not in self.employees:
+            self.employees.append(emp)
+    def remove_emp(self,emp):
+        if emp not in self.employees:
+            self.employees.remove(emp)
+    def print_emps(self):
+        for emp in self.employees:
+            print('------>', emp.fullname())
+            
+    
+    
+            
+        
 
-dev_1 = Developer('jonathan','davies', 50000)
-dev_2 = Developer('peter','davies', 600000)
+
+dev_1 = Developer('jonathan','davies', 50000,'python')
+dev_2 = Developer('peter','davies', 600000,'java')
+
+mgr_1 = Manager('Mr BIG', 'BIGGER', 10000000, [dev_1])
+print(mgr_1.email)
+
 
 print(dev_1.email)
+print(dev_2.prog_lang)
+
 
     
 
