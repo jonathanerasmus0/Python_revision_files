2989e496842d0d342752e409ef70f016e12cf4e6
Jonathan Davies <jonathan.davies@dci-student.org>
2024-02-13 11:05:51 +0100

    OOP

---
+++ Object_oriented_programming/getters_setters_deleters.py
@@ -0,0 +1,19 @@
+class Employee:
+    #initialise or CONSTRUCTOR  Instance variables 
+    
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + "." + last + "@hotmail.com"
+    def full_name(self):
+        return "{} {}".format(self.first, self.last)
+    
+
+emp_1 = Employee("jonathan", "davies", 50000)
+emp_2 = Employee("Peter", "davies",60000)
+
+print(emp_1.first)
+print(emp_1.email)
+
+#corey shafer 
