0b1b4bf8e38abc2eb38b4dc1f03c695e21a84984
Jonathan Davies <jonathan.davies@dci-student.org>
2024-02-20 14:27:07 +0100

    updates

--- Databases/exercise_05.py
+++ Databases/exercise_05.py
@@ -13,3 +13,5 @@ python helper.py --task make-csv --file task_5.csv --rows 100
 Try this with different number of rows so your program can take a longer time to run for example, 2000 rows.
 
 Profile the CPU usage of your computer as you run the instructions below. Keep checking on the statistics to know if the computer can really handle all this load. In Linux (and Mac), the command top can be used in the terminal to determine the CPU usage as you run scripts below.'''
+
+
