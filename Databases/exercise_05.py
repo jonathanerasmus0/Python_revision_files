f63edfd61a286a688c5f73fe5415a376fce4fca1
Jonathan Davies <jonathan.davies@dci-student.org>
2024-02-20 14:10:33 +0100

    updates

---
+++ Databases/exercise_05.py
@@ -0,0 +1,15 @@
+'''Dependencies for The Next Exercise.
+
+Please install the dependecies for some of the utility functions to run
+
+pip install -r requirements.txt
+
+Exercise 05: How to update a CSV based file
+
+In this exercise, execute a helper function we have written to write new content to a file.
+
+python helper.py --task make-csv --file task_5.csv --rows 100
+
+Try this with different number of rows so your program can take a longer time to run for example, 2000 rows.
+
+Profile the CPU usage of your computer as you run the instructions below. Keep checking on the statistics to know if the computer can really handle all this load. In Linux (and Mac), the command top can be used in the terminal to determine the CPU usage as you run scripts below.'''
